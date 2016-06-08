437c1d12d3ff5a0a73da093058861359db56c01b
Gustavo <theworldismine249@yahoo.com>
2016-06-09 00:41:27 +0300

    Initial commit

---
+++ src/test/java/com/mastercrafters/MastercraftersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mastercrafters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MastercraftersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
