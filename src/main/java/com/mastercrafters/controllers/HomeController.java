56640635e192b1d60396bca33cfdbcb57f715222
Gustavo <theworldismine249@yahoo.com>
2016-06-09 01:09:23 +0300

    good

---
+++ src/main/java/com/mastercrafters/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.mastercrafters.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Gustavo on 09/06/2016.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
